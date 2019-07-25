e7f39393f1813e51fbeced38cc82128e58b98e1d
ZEXSM <zaxabor@gmail.com>
2019-07-25 17:29:57 +0300

    #travis#coverage

--- coverage-code.sh
+++ coverage-code.sh
@@ -6,13 +6,11 @@ dotnet test --no-build test/**/*.csproj
 cd src/OData.QueryBuilder
 dotnet minicover uninstrument --workdir ../../coverage
 dotnet minicover report --workdir ../../coverage
+
+dotnet minicover coverallsreport \
+	--workdir ../../coverage \
+	--root-path ../../ \
+	--output "coveralls.json" \
+	--service-name "travis-ci" \
+	--service-job-id $TRAVIS_JOB_ID
 cd ../../
-echo ${TRAVIS_JOB_ID}
-# if [ -n "${TRAVIS_JOB_ID}" ]; then
-# 	dotnet minicover coverallsreport \
-# 		--workdir ../../coverage \
-# 		--root-path ../../ \
-# 		--output "coveralls.json" \
-# 		--service-name "travis-ci" \
-# 		--service-job-id "$TRAVIS_JOB_ID"
-# fi

c97529ee8fba3959d2da1b4fcb045fed563f9745
ZEXSM <zaxabor@gmail.com>
2019-07-25 17:59:35 +0300

    #travis#coverage

--- coverage-code.sh
+++ coverage-code.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-
-cd /src/OData.QueryBuilder
-dotnet minicover instrument --workdir ../../coverage --parentdir ../ --assemblies test/**/bin/${CONFIGURATION}/**/*.dll --sources src/**/*.cs
-dotnet minicover reset --workdir ../../coverage
-cd ../../
-dotnet test --no-build test/**/*.csproj
-cd /src/OData.QueryBuilder
-dotnet minicover uninstrument --workdir ../../coverage
-dotnet minicover report --workdir ../../coverage
-dotnet minicover coverallsreport --workdir ../../coverage --root-path ../../ --output "coveralls.json" --service-name "travis-ci" --service-job-id $TRAVIS_JOB_ID
-cd ../../
+cd src/OData.QueryBuilder \
+    && dotnet minicover instrument --workdir ../../coverage --parentdir ../ --assemblies test/**/bin/${CONFIGURATION}/**/*.dll --sources src/**/*.cs \
+    && dotnet minicover reset --workdir ../../coverage
+dotnet test --no-build ./test/OData.QueryBuilder.Test
+cd src/OData.QueryBuilder \
+    && dotnet minicover uninstrument --workdir ../../coverage \
+    && dotnet minicover report --workdir ../../coverage \
+    && dotnet minicover coverallsreport --workdir ../../coverage --root-path ../../ --output "coveralls.json" --service-name "travis-ci" --service-job-id $TRAVIS_JOB_ID

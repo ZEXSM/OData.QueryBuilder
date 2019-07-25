28b77ef0f01d42f696637e90325981dfe4cc3dba
ZEXSM <zaxabor@gmail.com>
2019-07-25 18:07:08 +0300

    #coverage

--- coverage-code.sh
+++ coverage-code.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 cd src/OData.QueryBuilder \
     && dotnet minicover instrument --workdir ../../coverage --parentdir ../ --assemblies test/**/bin/$CONFIGURATION/**/*.dll --sources src/**/*.cs \
-    && dotnet minicover reset --workdir ../../coverage
+    && dotnet minicover reset --workdir ../../coverage \
+    && cd ../../ \
 dotnet test --no-build ./test/OData.QueryBuilder.Test
 cd src/OData.QueryBuilder \
     && dotnet minicover uninstrument --workdir ../../coverage \
     && dotnet minicover report --workdir ../../coverage \
-    && dotnet minicover coverallsreport --workdir ../../coverage --root-path ../../ --output "coveralls.json" --service-name "travis-ci" --service-job-id $TRAVIS_JOB_ID
+    && dotnet minicover coverallsreport --workdir ../../coverage --root-path ../../ --output "coveralls.json" --service-name "travis-ci" --service-job-id $TRAVIS_JOB_ID \
+    && cd ../../ \

5d257a5985122b9df652b97bb08266d02a5b9711
ZEXSM <zaxabor@gmail.com>
2019-07-25 17:02:21 +0300

    #coverage

--- coverage-code.sh
+++ coverage-code.sh
@@ -1,23 +1,13 @@
-cd src/OData.QueryBuilder
-
+cd ./src/OData.QueryBuilder
 dotnet minicover instrument --workdir ../../coverage --parentdir ../ --assemblies test/**/bin/${CONFIGURATION}/**/*.dll --sources src/**/*.cs
-
 dotnet minicover reset --workdir ../../coverage
-
 cd ../../
-
 dotnet test --no-build test/**/*.csproj
-
-cd src/OData.QueryBuilder
-
+cd ./src/OData.QueryBuilder
 dotnet minicover uninstrument --workdir ../../coverage
-
 dotnet minicover report --workdir ../../coverage
-
 cd ../../
-
 echo ${TRAVIS_JOB_ID}
-
 # if [ -n "${TRAVIS_JOB_ID}" ]; then
 # 	dotnet minicover coverallsreport \
 # 		--workdir ../../coverage \

8500ea45cc37b3ff046371e68e4b7f337b19a069
ZEXSM <zaxabor@gmail.com>
2020-05-11 19:03:16 +0300

    #ZEX#fix

--- src/OData.QueryBuilder/Expressions/ExpandExpression.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace OData.QueryBuilder.Expressions
-{
-    public class ExpandExpression<T> : Expression
-    {
-    }
-}
--- src/OData.QueryBuilder/Expressions/OrderByExpression.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace OData.QueryBuilder.Expressions
-{
-    public sealed class OrderByExpression : Expression
-    {
-    }
-}

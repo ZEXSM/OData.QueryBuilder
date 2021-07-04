44451a5164d789582c265f63550d5e961dd45f90
ZEXSM <zaxabor@gmail.com>
2021-07-04 23:49:04 +0300

    add support composite keys

--- src/OData.QueryBuilder/Conventions/AddressingEntities/IAddressingEntries.cs
+++ src/OData.QueryBuilder/Conventions/AddressingEntities/IAddressingEntries.cs
@@ -5,11 +5,11 @@ namespace OData.QueryBuilder.Conventions.AddressingEntities
 {
     public interface IAddressingEntries<TEntity>
     {
-        IODataQueryKey<TEntity> ByKey(int key);
+        IODataQueryKey<TEntity> ByKey(params int[] keys);
 
-        IODataQueryKey<TEntity> ByKey(string key);
+        IODataQueryKey<TEntity> ByKey(params string[] keys);
 
-        IODataQueryKey<TEntity> ByKey(Guid key);
+        IODataQueryKey<TEntity> ByKey(params Guid[] keys);
 
         IODataQueryCollection<TEntity> ByList();
     }

1b1238a541305dd08a3ef08eb6d95b4c22c6a58d
ZEXSM <zaxabor@gmail.com>
2021-10-07 18:29:59 +0300

    add apply

--- src/OData.QueryBuilder/Conventions/AddressingEntities/Options/IODataOptionCollection.cs
+++ src/OData.QueryBuilder/Conventions/AddressingEntities/Options/IODataOptionCollection.cs
@@ -13,6 +13,9 @@ namespace OData.QueryBuilder.Conventions.AddressingEntities.Options
 
         TODataOption Filter(Expression<Func<TEntity, IODataFunction, IODataOperator, bool>> filter, bool useParenthesis = false);
 
+        http://docs.oasis-open.org/odata/odata-data-aggregation-ext/v4.0/cs01/odata-data-aggregation-ext-v4.0-cs01.html#_Toc378326289
+        TODataOption Apply(Expression<Func<TEntity, bool>> apply);
+
         TODataOption OrderBy(Expression<Func<TEntity, object>> orderBy);
 
         TODataOption OrderBy(Expression<Func<TEntity, ISortFunction, object>> orderBy);
